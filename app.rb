4214634eb749e6ab4713c2d97ab31088449d1d36
anderslemke <mail@anderslemke.dk>
2020-04-10 21:02:55 +0200

    Go on

--- app.rb
+++ app.rb
@@ -7,21 +7,32 @@ require 'json/jwt'
 require 'net/https'
 require 'httparty'
 
+get '/' do
+  erb :go_on
+end
+
 get '/authenticate' do
   string = params[:id_token]
-  payload, header = JWT.decode(string , nil, false)
-  jwks_url = "https://#{payload['iss']}/.well-known/jwks.json"
-  jwks = JSON.parse(HTTParty.get(jwks_url).body)
+  if string
+    payload, header = JWT.decode(string , nil, false)
+    jwks_url = "https://#{payload['iss']}/.well-known/jwks.json"
+    jwks = JSON.parse(HTTParty.get(jwks_url).body)
 
-  jwk = jwks["keys"].first
-  key = JSON::JWK.new(jwk).to_key
-  decoded_token = JWT.decode string, key, true, { algorithm: header['alg'] }
+    jwk = jwks["keys"].first
+    key = JSON::JWK.new(jwk).to_key
+    decoded_token = JWT.decode string, key, true, { algorithm: header['alg'] }
 
-  erb :index, locals: {
-    payload: payload,
-    header: header,
-    jwks_url: jwks_url,
-    jwks: jwks,
-    decoded_token: decoded_token
-  }
+    user_id = [payload['iss'], payload['sub']].join('|')
+
+    erb :index, locals: {
+      payload: payload,
+      header: header,
+      jwks_url: jwks_url,
+      jwks: jwks,
+      decoded_token: decoded_token,
+      user_id: user_id
+    }
+  else
+    erb :go_on
+  end
 end

06c3f11406efad6dea2fa4afc56054987eb7794c
anderslemke <mail@anderslemke.dk>
2020-04-13 15:58:41 +0200

    Name an ui

--- app.rb
+++ app.rb
@@ -48,7 +48,7 @@ end
 get '/.well-known/authentication.json' do
   content_type 'application/json'
   {
-    name: 'Ruby Sample',
+    name: 'Ruby App',
     logo_url: 'http://pngimg.com/uploads/ruby/ruby_PNG47.png'
   }.to_json
 end
